29baa7975beac42ffc4167eea81db5d7dc2feb66
Heitor <heitor.moreira143@gmail.com>
2026-07-28 21:21:37 -0300

    Initial Commit

---
+++ multiplica numeros/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  multiplica numeros
+//
+//  Created by Heitor Moreira on 28/07/26.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return EXIT_SUCCESS;
+}
