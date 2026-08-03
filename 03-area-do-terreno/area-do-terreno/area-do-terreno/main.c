7ec040298caf159dd9b23f817843a3a8e1e1c55e
Heitor <heitor.moreira143@gmail.com>
2026-08-03 06:55:08 -0300

    O programa recebe a variável inserida pelo usuário para calcular a largura de um terreno, e após isso, calcula a área desse mesmo terreno

---
+++ 03-area-do-terreno/area-do-terreno/area-do-terreno/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  area-do-terreno
+//
+//  Created by Heitor Moreira on 03/08/26.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return EXIT_SUCCESS;
+}
