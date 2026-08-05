010bad1076c2bb517308a53cbd51b58742a97ab8
Heitor <heitor.moreira143@gmail.com>
2026-08-04 22:00:04 -0300

    fix: corrigido o erro de cálculo da quantidade de fotos

--- 04-cobrir-parede/cobrir-a-parede-de-fotos/cobrir-a-parede-de-fotos/main.c
+++ 04-cobrir-parede/cobrir-a-parede-de-fotos/cobrir-a-parede-de-fotos/main.c
@@ -7,17 +7,16 @@
 
 int main (void)
 {
-    int largura_foto = 0.5;
-    int comprimento_foto  = 0.8;
-    int area_foto = largura_foto * comprimento_foto;
+    float area_foto = 0.4;
     float largura_parede = 0.0;
     float comprimento_parede = 0.0;
-    printf("Insira a largura da sua parede: \n");
-    scanf("%f.2", &largura_parede);
-    printf("Insira o comprimento da parede: \n");
-    scanf("%f.2", &comprimento_parede);
+    printf("Insira a largura da sua parede: ");
+    scanf("%f", &largura_parede);
+    printf("Insira o comprimento da parede: ");
+    scanf("%f", &comprimento_parede);
     float area_parede = largura_parede * comprimento_parede;
     float qtd_fotos = area_parede / area_foto;
-    printf("A quantidade de fotos que você precisa é de: %f.2\n", qtd_fotos);
+    printf("A quantidade de fotos que você precisa é de: %2.f\n", qtd_fotos);
     return 0;
 }
+
